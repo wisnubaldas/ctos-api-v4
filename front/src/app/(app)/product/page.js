2ca96917b45c6c60777ae18187f688d37a6cb112
wisnu baldas <wisnubaldas@gmail.com>
2025-01-13 22:06:40 +0700

    front

---
+++ front/src/app/(app)/product/page.js
@@ -0,0 +1,24 @@
+import Header from '@/app/(app)/Header'
+
+export const metadata = {
+    title: 'Product - Laravel',
+}
+
+const Product = () => {
+    return (
+        <>
+            <Header title="Dashboard" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                            Haloo Ini halaman Product
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Product
---
+++ front/src/app/page.js
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+const Home = () => {
+    redirect('/login'); // Redirect langsung ke /loginini
+}
+
+export default Home
